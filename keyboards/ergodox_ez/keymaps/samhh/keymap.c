e9408f0774a31f99ab5e2209cfd96cf65f06de8c
Sam A. Horvath-Hunt <git@samhh.com>
2020-01-21 17:06:54 +0000

    Ensure consistent spacing

--- keyboards/ergodox_ez/keymaps/samhh/keymap.c
+++ keyboards/ergodox_ez/keymaps/samhh/keymap.c
@@ -163,52 +163,52 @@ qk_tap_dance_action_t tap_dance_actions[] = {
 //      2: Green
 //      3: Blue
 layer_state_t layer_state_set_user(layer_state_t state) {
-  ergodox_board_led_off();
-  ergodox_right_led_1_off();
-  ergodox_right_led_2_off();
-  ergodox_right_led_3_off();
+    ergodox_board_led_off();
+    ergodox_right_led_1_off();
+    ergodox_right_led_2_off();
+    ergodox_right_led_3_off();
 
-  uint8_t layer = get_highest_layer(state);
-  switch (layer) {
-      case 0:
-        break;
+    uint8_t layer = get_highest_layer(state);
+    switch (layer) {
+        case 0:
+            break;
 
-      case 1:
-        ergodox_right_led_3_on();
-        break;
+        case 1:
+            ergodox_right_led_3_on();
+            break;
 
-      case 2:
-        ergodox_right_led_2_on();
-        break;
+        case 2:
+            ergodox_right_led_2_on();
+            break;
 
-      case 3:
-        ergodox_right_led_1_on();
-        break;
-      case 4:
-        ergodox_right_led_1_on();
-        ergodox_right_led_2_on();
-        break;
+        case 3:
+            ergodox_right_led_1_on();
+            break;
+        case 4:
+            ergodox_right_led_1_on();
+            ergodox_right_led_2_on();
+            break;
 
-      case 5:
-        ergodox_right_led_1_on();
-        ergodox_right_led_3_on();
-        break;
+        case 5:
+            ergodox_right_led_1_on();
+            ergodox_right_led_3_on();
+            break;
 
-      case 6:
-        ergodox_right_led_2_on();
-        ergodox_right_led_3_on();
-        break;
+        case 6:
+            ergodox_right_led_2_on();
+            ergodox_right_led_3_on();
+            break;
 
-      case 7:
-        ergodox_right_led_1_on();
-        ergodox_right_led_2_on();
-        ergodox_right_led_3_on();
-        break;
+        case 7:
+            ergodox_right_led_1_on();
+            ergodox_right_led_2_on();
+            ergodox_right_led_3_on();
+            break;
 
-      default:
-        break;
+        default:
+            break;
     }
 
-  return state;
+    return state;
 };
 
